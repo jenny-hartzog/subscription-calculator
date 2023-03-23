da911e676f03c5b6b390bc70783a55181ac8541e
Jenny Hartzog <jenny@nomorebeets>
2023-03-23 13:20:59 -0700

    Added JS event listeners, var, & conditional blocks

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-// Your JS code here
+let subTypeElement = document.querySelector("select#subscription");
+let subDurationElement = document.querySelector("select#months");
+let result = document.querySelector(".result");
+let subType = "basic";
+let subDuration = 1;
+console.log(subTypeElement);
+console.log (subDurationElement);
+console.log(result);
+console.log(subType);
+console.log(subDuration);
+
+subTypeElement.addEventListener("change", function(e){
+    subType = e.target.value;
+    updateSubscriptionDiv();
+    //console.log(subType);
+})
+
+subDurationElement.addEventListener("change", function(e){
+    subDuration = Number(e.target.value);
+    updateSubscriptionDiv();
+    //console.log(subDuration);
+})
+
+let updateSubscriptionDiv = function(){
+    let monthlyCost = 5;
+    if (subType === "standard"){
+        monthlyCost = 7;
+    }else if(subType === "premium"){
+        monthlyCost = 10;
+    }
+    let total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+}
+
